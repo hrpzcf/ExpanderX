35bb4cf5546503f5e4e810108ca94347b0fac681
hrpzcf <pphrp@qq.com>
2022-06-19 16:34:26 +0800

    版本：1.2.0.220619。

--- ExpanderX/Information.cs
+++ ExpanderX/Information.cs
@@ -2,9 +2,9 @@
 {
     internal static class Information
     {
-        public const int Date = 220611;
+        public const int Date = 220619;
         public const int Major = 1;
-        public const int Minor = 1;
+        public const int Minor = 2;
         public const int Micro = 0;
         public static string VER
         {
--- ExpanderX/Settings.cs
+++ ExpanderX/Settings.cs
@@ -3,7 +3,6 @@ using System;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
-using static System.Environment;
 
 namespace ExpanderX
 {
